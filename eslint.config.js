d0270a8c182c68b763383218a7456ab030c83870
zhaiyuxin103 <zhaiyuxin103@hotmail.com>
2024-04-19 17:42:41 +0800

    build(deps): add eslint (#26)

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+    { languageOptions: { globals: globals.browser } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        ignores: [
+            'docs/.vitepress/cache',
+            'docs/.vitepress/dist',
+            'public/build',
+            'public/js/filament',
+            'public/vendor',
+            'vendor',
+        ],
+    },
+];
--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,4 +1,5 @@
 export default {
     '*.php': ['./vendor/bin/pint'],
     '*.{js,mjs,jsx,ts,tsx,json,css,scss,md,vue}': ['prettier . --write'],
+    '*.{js, jsx,ts,tsx}': ['eslint --fix .'],
 };
