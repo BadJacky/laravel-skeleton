778bfb981c5fb74ee180044a078565cbf2ceeb98
zhaiyuxin103 <zhaiyuxin103@hotmail.com>
2024-08-08 13:17:55 +0800

    perf(api): 频率限制 (#34)

--- routes/api.php
+++ routes/api.php
@@ -7,4 +7,12 @@ Route::middleware(['change-locale'])->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     })->middleware('auth:sanctum');
+
+    Route::middleware(['throttle:'.config('api.rate_limits.sign')])->group(function () {
+        //
+    });
+
+    Route::middleware('throttle:'.config('api.rate_limits.access'))->group(function () {
+        //
+    });
 });
